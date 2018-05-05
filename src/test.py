67cf1954d9a5c6e4727e0e8ad0dd80ed56dc1551
Omar Rasheed <36868927+Omarrasheed@users.noreply.github.com>
2018-05-04 22:25:25 -0700

    Add files via upload

--- src/test.py
+++ src/test.py
@@ -5,6 +5,61 @@ from sqlalchemy import inspect
 from flask import Flask, jsonify
 from app import app,db,base
 from sqlalchemy.orm import joinedload
+import requests
+import json
+
+titleList = []
+allClasses = []
+
+# Pull all subjects and append to titleList
+response = requests.get("https://classes.cornell.edu/api/2.0/config/subjects.json?roster=FA18")
+data = response.json()
+for each in data['data']['subjects']:
+    titleList.append(str(each['value']))
+
+def pullInfoForSubject(subj):
+    request = "https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA18&subject=" + subj
+    response = requests.get(request)
+    data = response.json()
+    for each in data['data']['classes']:
+        classObject = []
+        newSubject = each['subject'].encode("ascii", 'ignore')
+        classObject.append(newSubject)
+        newNbr = each['catalogNbr'].encode("ascii", 'ignore')
+        classObject.append(newNbr)
+        newTitle = each['titleLong'].encode('ascii', 'ignore')
+        classObject.append(newTitle)
+        if each['description'] != None:
+            new = each['description'].encode("ascii", 'ignore')
+            classObject.append(new)
+        else:
+            classObject.append(" ")
+        
+        if each['catalogWhenOffered'] != None:
+            new = each['catalogWhenOffered'].encode("ascii", 'ignore')
+            classObject.append(new)
+        else:
+            classObject.append(" ")
+        
+        if each["enrollGroups"][0]["unitsMaximum"] != None:
+            classObject.append(each["enrollGroups"][0]["unitsMaximum"])
+        else:
+            classObject.append(" ")
+        
+        if each["enrollGroups"][0]["unitsMinimum"] != None:
+            classObject.append(each["enrollGroups"][0]["unitsMinimum"])
+        else:
+            classObject.append(" ")
+        
+        if each["catalogPrereqCoreq"] != "":
+            classObject.append(each['catalogPrereqCoreq'])
+        else:
+            classObject.append(" ")
+        allClasses.append(classObject)
+            
+for each in titleList:
+    pullInfoForSubject(each)
+print(allClasses)
 
 class test(unittest.TestCase):
   coursePostColumns = [
@@ -16,7 +71,6 @@ class test(unittest.TestCase):
     'creditsMax',
     'term',
     'prereqs',
-    'id'
   ]
 
   boardGetAllColumns = [
@@ -78,75 +132,28 @@ class test(unittest.TestCase):
     self.app_context.push()
 
   def tearDown(self):
-    db.session.execute('DELETE FROM courses;')
+    db.session.execute('DELETE FROM course;')
     self.commit()
     self.app_context.pop()
-    
-  
 
   def test_create_board(self):
     
-    titleList = []
-    allClasses = []
 
-    # Pull all subjects and append to titleList
-    response = requests.get("https://classes.cornell.edu/api/2.0/config/subjects.json?roster=SP18")
-    data = response.json()
-    for each in data['data']['subjects']:
-	    titleList.append(str(each['value']))
 
-    def pullInfoForSubject(subj):
-	    request = "https://classes.cornell.edu/api/2.0/search/classes.json?roster=SP18&subject=" + subj
-	    response = requests.get(request)
-	    data = response.json()
-	    for each in data['data']['classes']:
-		    classObject = []
-		    classObject.append(each['subject'].encode("utf-8"))
-		    classObject.append(each['catalogNbr'].encode("utf-8"))
-		    classObject.append(each['titleLong'].encode("utf-8"))
-		    if each['description'] != None:
-			    classObject.append(each['description'].encode("utf-8"))
-		    else:
-			    classObject.append(None)
-
-		    if each['catalogWhenOffered'] != None:
-			    classObject.append(each["catalogWhenOffered"].encode("utf-8"))
-		    else:
-			    classObject.append(None)
-
-		    if each["enrollGroups"][0]["unitsMaximum"] != None:
-			    classObject.append(each["enrollGroups"][0]["unitsMaximum"])
-		    else:
-			    classObject.append(None)
-
-		    if each["enrollGroups"][0]["unitsMinimum"] != None:
-			    classObject.append(each["enrollGroups"][0]["unitsMinimum"])
-		    else:
-			    classObject.append(None)
-
-		    if each["catalogPrereqCoreq"] != "":
-			    classObject.append(each['catalogPrereqCoreq'])
-		    else:
-			    classObject.append(None)
-		    allClasses.append(classObject)
-
-      for each in titleList:
-	      pullInfoForSubject(each)
-    
     for each in allClasses:
-      input_data = dict(title=each[2],
-                        subject=each[0],
-                        number=each[1],
-                        description=each[3],
-                        term=each[4],
-                        creditsMax=each[5],
-                        creditsMin=each[6],
-                        prereqs=each[7])
-      result = json.loads(self.post(input_data, 'courses').data)
-      assert(self.is_sub(self.coursePostColumns,result['data']['course'].keys()))
-      assert(result['success'])
+        input_data = dict(title=each[2],
+                          subject=each[0],
+                          number=each[1],
+                          description=each[3],
+                          term=each[4],
+                          creditsMax=each[5],
+                          creditsMin=each[6],
+                          prereqs=each[7])
+        result = json.loads(self.post(input_data, 'courses').data)
+        assert(self.is_sub(self.coursePostColumns,result['data']['course'].keys()))
+        assert(result['success'])
 
-  def test_delete_board(self):
+  """def test_delete_board(self):
     input_data = dict(title='My Awesome Board')
     result = json.loads(self.post(input_data, 'courses').data)
     result_id = int(result['data']['board']['id'])
@@ -159,42 +166,28 @@ class test(unittest.TestCase):
     assert(not has_board)
 
   def test_get_boards(self):
-    input_data1 = dict(
-      title='My Awesome Board')
-    input_data2 = dict(
-      title='My Awesome Board 2')
-    result_id1 = json.loads(self.post(input_data1, 'courses').data)['data']['board']['id']
-    result_id2 = json.loads(self.post(input_data2, 'courses').data)['data']['board']['id']
-    input_data1 = dict(
-      board_id=result_id1,
-      description='A Todo Task, I should get this done!',
-      category='inprogress')
-    input_data2 = dict(
-      board_id=result_id1,
-      description='A Todo Task, I should get this done!',
-      category='inprogress')
-    input_data3 = dict(
-      board_id=result_id2,
-      description='A Todo Task, I should get this done!',
-      category='todo')
-    input_data4 = dict(
-      board_id=result_id2,
-      description='A Todo Task, I should get this done!',
-      category='done')
-    self.post(input_data1, 'board_elements')
-    self.post(input_data2, 'board_elements')
-    self.post(input_data3, 'board_elements')
-    self.post(input_data4, 'board_elements')
+    input_data1 = dict(title='title',
+                      subject='subject',
+                      number='number',
+                      description='description',
+                      term='term',
+                      creditsMax=1,
+                      creditsMin=0,
+                      prereqs='prereqs')
+    input_data = dict(title='title2',
+                      subject='subject2',
+                      number='number2',
+                      description='description2',
+                      term='term2',
+                      creditsMax=12,
+                      creditsMin=0,
+                      prereqs='prereqs2')
+    result_id1 = json.loads(self.post(input_data1, 'courses').data)['data']['course']['id']
+    result_id2 = json.loads(self.post(input_data, 'courses').data)['data']['course']['id']
     result = json.loads(self.app.get('/planner/courses').data)
-    boards = result['data']['boards']
-    assert(self.is_sub(self.boardGetAllColumns,boards[0].keys()))
+    boards = result['data']['courses']
+    assert(self.is_sub(self.coursePostColumns,boards[0].keys()))
     assert(result['success'])
-    assert(result['data']['boards'][0]['todo_count']) == 0
-    assert(result['data']['boards'][0]['inprogress_count']) == 2
-    assert(result['data']['boards'][0]['done_count']) == 0
-    assert(result['data']['boards'][1]['todo_count']) == 1
-    assert(result['data']['boards'][1]['inprogress_count']) == 0
-    assert(result['data']['boards'][1]['done_count']) == 1
 
   def test_get_board(self):
     input_data = dict(title='My Awesome Board')
@@ -263,7 +256,7 @@ class test(unittest.TestCase):
     result = json.loads(self.app.get('/kanban/boards').data)
     assert(result['data']['boards'][0]['todo_count'] == 0)
     assert(result['data']['boards'][0]['inprogress_count'] == 3)
-    assert(result['data']['boards'][0]['done_count'] == 1)
+    assert(result['data']['boards'][0]['done_count'] == 1)"""
 
 if __name__ == '__main__':
   unittest.main()
