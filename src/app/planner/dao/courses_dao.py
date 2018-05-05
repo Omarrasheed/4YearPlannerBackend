56306c8d4d77c52be4f5a34ed57666d4e4e1f8ff
Omar Rasheed <36868927+Omarrasheed@users.noreply.github.com>
2018-05-04 23:26:10 -0700

    Add files via upload

--- src/app/planner/dao/courses_dao.py
+++ src/app/planner/dao/courses_dao.py
@@ -5,11 +5,34 @@ from . import *
 Add more methods below!!!
 """
 
-def course_by_title(course_title):
+def courses_by_subject(course_subject):
   	"""
   	Get board by ID
   	"""
-  	return Course.query.filter_by(title=course_title).first()
+  	return Course.query.filter_by(subject=course_subject)
+
+def courses_by_term(term):
+	"""
+	Get Course by Subject
+	"""
+	finalList = []
+	realFall = "Fall"
+	realSpring = "Spring"
+	secondSpring = "spring"
+
+	query = Course.query.all()
+	for each in query:
+		if term == 'fall':
+			if (realFall in each.term):
+				finalList.append(each)
+		elif term == 'spring':
+			if (realSpring in each.term) or (secondSpring in each.term):
+				finalList.append(each)
+		elif term == 'fall and spring':
+			if (realFall in each.term) or (realSpring in each.term) or (secondSpring in each.term) :
+				finalList.append(each)
+	return finalList
+
 
 def all_courses():
 	return Course.query.all()
