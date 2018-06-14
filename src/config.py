32395437680707d80d83098ed70479e2be6b1530
Omar Rasheed <36868927+Omarrasheed@users.noreply.github.com>
2018-06-13 20:17:54 -0700

    Forgot to take out some things

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 import os
 
 # Database info
-DB_USERNAME  = os.environ['PLANNER_DB_USERNAME']
-DB_PASSWORD  = os.environ['PLANNER_DB_PASSWORD']
-DB_HOST      = os.environ['PLANNER_DB_HOST']
-DB_NAME      = os.environ['PLANNER_DB_NAME']
+DB_USERNAME  = os.environ['DB_USERNAME']
+DB_PASSWORD  = os.environ['DB_PASSWORD']
+DB_HOST      = os.environ['DB_HOST']
+DB_NAME      = os.environ['DB_NAME']
 DB_URL       = 'mysql://{}:{}@{}/{}'.format(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
 
 # Ensure Python path
